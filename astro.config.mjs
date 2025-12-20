af00b202fe1356113f6dd9acad366e4011b7292b
Kwensiu <kwen.siu@gmail.com>
2025-12-20 23:17:44 +0800

    feat(config): 更新站点配置为新域名和路径

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,8 +26,8 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://kwensiu.github.io/mSpace",
-	base: "/mSpace",
+	site: "https://kwensiu.github.io/blog",
+	base: "/blog",
 	trailingSlash: "always",
 
 	integrations: [
